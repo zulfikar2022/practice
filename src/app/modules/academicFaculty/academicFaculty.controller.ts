8aeb00d7272a0c327868e3b5a3e866d4abd66196
zulfikar2022 <sayedzulfikar2019@gmail.com>
2024-12-03 05:44:23 +0600

    Implement academic faculty controller with CRUD operations; add error handling using catchAsync

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -1,1 +1,48 @@
+import { catchAsync } from '../../utils/catchAsync';
+import { academicFacultyServices } from './academicFaculty.service';
 
+const createAcademicFaculty = catchAsync(async (req, res) => {
+  const academicFaculty =
+    await academicFacultyServices.createAcademicFacultyIntoDB(req.body);
+  res.status(201).json({
+    status: 'Academic faculty created successfully',
+    data: academicFaculty,
+  });
+});
+
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const academicFaculties =
+    await academicFacultyServices.getAllAcademicFacultyFromDB();
+  res.status(200).json({
+    status: 'All academic faculties fetched successfully',
+    data: academicFaculties,
+  });
+});
+
+const getSingleAcademicFaculty = catchAsync(async (req, res) => {
+  const academicFaculty =
+    await academicFacultyServices.getSingleAcademicFacultyFromDB(req.params.id);
+  res.status(200).json({
+    status: 'Academic faculty fetched successfully',
+    data: academicFaculty,
+  });
+});
+
+const updateAcademicFaculty = catchAsync(async (req, res) => {
+  const updatedAcademicFaculty =
+    await academicFacultyServices.updateAcademicFacultyIntoDB(
+      req.params.id,
+      req.body,
+    );
+  res.status(200).json({
+    status: 'Academic faculty updated successfully',
+    data: updatedAcademicFaculty,
+  });
+});
+
+export const academicFacultyControllers = {
+  createAcademicFaculty,
+  getAllAcademicFaculties,
+  getSingleAcademicFaculty,
+  updateAcademicFaculty,
+};
