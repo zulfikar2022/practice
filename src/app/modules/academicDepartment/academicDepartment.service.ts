6c6b93bc42c728410d4a42d2cdd37b30c1fc0fe5
zulfikar2022 <sayedzulfikar2019@gmail.com>
2024-12-03 07:00:16 +0600

    Implement academic department service with CRUD operations

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -1,1 +1,36 @@
+import { TAcadmicDepartment } from './academicDepartment.interface';
+import { AcademicDepartment } from './academicDepartment.model';
 
+const createAcademicDepartment = async (payload: TAcadmicDepartment) => {
+  return await AcademicDepartment.create(payload);
+};
+
+const getAllAcademicDepartments = async () => {
+  return await AcademicDepartment.find();
+};
+const getSpecificAcademicDepartment = async (id: string) => {
+  return await AcademicDepartment.findById({ _id: id });
+};
+
+const updateAcademicDepartment = async (
+  id: string,
+  payload: TAcadmicDepartment,
+) => {
+  return await AcademicDepartment.findByIdAndUpdate(
+    { _id: id },
+    {
+      $set: {
+        name: payload.name,
+        academicFaculty: payload.academicFaculty,
+      },
+    },
+    { new: true },
+  );
+};
+
+export const academicDepartmentService = {
+  createAcademicDepartment,
+  getAllAcademicDepartments,
+  getSpecificAcademicDepartment,
+  updateAcademicDepartment,
+};
