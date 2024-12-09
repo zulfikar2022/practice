303ae4994f694409a7d6d1d47adc3a2039e606fb
zulfikar2022 <sayedzulfikar2019@gmail.com>
2024-12-09 19:44:15 +0600

    Add course model and interface: define TCourse and TPreRequisiteCourse types; implement Mongoose schemas for course management

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import { TCourse, TPreRequisiteCourse } from './course.interface';
+
+const preRequisiteCoursesSchema = new mongoose.Schema<TPreRequisiteCourse>({
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const courseSchema = new mongoose.Schema<TCourse>({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  prefix: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  code: {
+    type: Number,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+  },
+  preRequisiteCourses: [preRequisiteCoursesSchema],
+});
+
+export const Course = mongoose.model<TCourse>('Course', courseSchema);
