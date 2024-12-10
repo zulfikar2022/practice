7b8b6486587b0a931f39aedad3441001a6f6be2d
zulfikar2022 <sayedzulfikar2019@gmail.com>
2024-12-10 07:13:36 +0600

    Enhance course update functionality: implement handling for prerequisite courses, including adding new and removing deleted prerequisites in updateCourse method

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -63,7 +63,43 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
       courseRemainingData,
       { new: true, runValidators: true },
     );
-    return updateBasicCourseInfo;
+    let newPrerequisiteCourses;
+    let deletedPrerequisiteCourses;
+    // check if there is any prerequisite course to update
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+      // filter the deleted fields from the payload
+      const deletedPrerequisites = preRequisiteCourses
+        .filter((course) => course.isDeleted)
+        .map((el) => el.course);
+
+      // filtering out the new course fields
+      const newPrerequisites = preRequisiteCourses.filter(
+        (course) => !course.isDeleted,
+      );
+      console.log(newPrerequisites);
+
+      const newPrerequisiteCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $addToSet: { preRequisiteCourses: { $each: newPrerequisites } },
+        },
+        { new: true },
+      );
+
+      deletedPrerequisiteCourses = await Course.findByIdAndUpdate(
+        id,
+        {
+          $pull: {
+            preRequisiteCourses: { course: { $in: deletedPrerequisites } },
+          },
+        },
+        { new: true },
+      );
+    }
+    const result = await Course.findById(id).populate(
+      'preRequisiteCourses.course',
+    );
+    return result;
   } catch (error) {
     throw new Error((error as Error).message);
   }
